94fe3c9f5db4bd5af2b723bdeff6520c5bfb31cc
cloudsystems22 <cloudsystems22@gmail.com>
2021-08-11 09:55:30 -0300

    Aggregate method tutorial

---
+++ MethodAggregate/MethodAggregate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace MethodAggregate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[]{ 1, 2, 3, 4, 5 };
+            var sum = numbers.Aggregate(
+                0,
+                (total, num) =>
+                {
+                    Console.WriteLine("Total = {0} + {1}", total, num);
+                    return total + num;
+                }
+            );
+            Console.WriteLine($"Valor total = {sum}");
+
+        }
+    }
+}
