6c7d1f2cc6dd0be2bfa49b8d05c7ad50df4a3316
cloudsystems22 <cloudsystems22@gmail.com>
2021-08-11 17:32:54 -0300

    Relational Pattner

---
+++ RelationalPatterns/RelationalPatterns/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelationalPatterns
+{
+    enum Generation { BabyBoomer, GenX, Millenial, GenZ, GenA }
+
+    class Person
+    {
+        public int BirthYear { get; set; }
+
+        public Generation Generation =>
+            BirthYear switch
+            {
+                (>= 1946) and (<= 1964) => Generation.BabyBoomer,
+                (>= 1965) and (<= 1980) => Generation.GenX,
+                (>= 1981) and (<= 1996) => Generation.Millenial,
+                (>= 1997) and (<= 2012) => Generation.GenZ,
+                _ => Generation.GenA
+            };
+
+    }
+}
---
+++ RelationalPatterns/RelationalPatterns/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RelationalPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person { BirthYear = 1950 };
+            Console.WriteLine($"Resultado: {p.Generation}");
+        }
+    }
+}
