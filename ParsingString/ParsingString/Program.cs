f54587830a9c8df123b8c430280ac279c4d73a92
cloudsystems22 <cloudsystems22@gmail.com>
2021-08-12 12:12:24 -0300

    ParsingString

---
+++ ParsingString/ParsingString/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace ParsingString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Wage();
+            HireDate();
+            Console.ReadLine();
+        }
+
+        private static void HireDate()
+        {
+            string hireDateString = "12/08/2021";
+            DateTime hireDate = DateTime.Parse(hireDateString);
+            Console.WriteLine($"Parse date: {hireDate}");
+
+            var cultureInfo = new CultureInfo("pt-BR");
+            string birthDateString = "22 maio 1978";
+            var birthDate = DateTime.Parse(birthDateString, cultureInfo);
+            Console.WriteLine($"Date birth: {birthDate}");
+        }
+
+        private static void Wage()
+        {
+            Console.WriteLine("Enter wage:");
+            string wage = Console.ReadLine();
+
+            int wageValue;
+
+            if (int.TryParse(wage, out wageValue))
+                Console.WriteLine($"Parsing sucess: {wageValue}");
+            else
+                Console.WriteLine($"Parsing failed!");
+
+        }
+    }
+}
--- RelationalPatterns/RelationalPatterns/Program.cs
+++ RelationalPatterns/RelationalPatterns/Program.cs
@@ -5,7 +5,7 @@ namespace RelationalPatterns
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
             var p = new Person { BirthYear = 1950 };
             Console.WriteLine($"Resultado: {p.Generation}");
         }
